3c8f0eed13561036fcbe565b9d0e53a4441109e6
Jacob Tomaw <jacob.tomaw@gmail.com>
2018-04-19 16:59:39 -0400

    Solved 16-20

--- src/ii_collections/n16FlatMap.kt
+++ src/ii_collections/n16FlatMap.kt
@@ -1,5 +1,7 @@
 package ii_collections
 
+import v_builders.data.getProducts
+
 fun example() {
 
     val result = listOf("abc", "12").flatMap { it.toList() }
@@ -7,12 +9,6 @@ fun example() {
     result == listOf('a', 'b', 'c', '1', '2')
 }
 
-val Customer.orderedProducts: Set<Product> get() {
-    // Return all products this customer has ordered
-    todoCollectionTask()
-}
+val Customer.orderedProducts: Set<Product> get() = orders.flatMap { it.products }.toSet()
 
-val Shop.allOrderedProducts: Set<Product> get() {
-    // Return all products that were ordered by at least one customer
-    todoCollectionTask()
-}
+val Shop.allOrderedProducts: Set<Product> get() = customers.flatMap {it.orders }.flatMap { it.products }.toSet()
